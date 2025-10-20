21ab4808f163db19d3f98064c3b84f70a3816d9a
Marciano <m.f.h.h.g.brouwer@student.utwente.nl>
2025-10-20 11:33:00 +0200

    started on basic networking functionality

--- src/raspberryPi/main.py
+++ src/raspberryPi/main.py
@@ -1,6 +1,24 @@
 import asyncio
 import websockets
+import json
+
+url = "https://local"
+
+async def listen():
+    
+    async with websockets.connect(url) as socket:
+
+        while True:
+            received = socket.recv()
+            processReception(received)
 
 
-asyn
+asyncio.get_event_loop().run_until_complete(listen)    
 
+
+def processReception(received):
+    package = json.dumps(received)
+
+
+    pass
+    
